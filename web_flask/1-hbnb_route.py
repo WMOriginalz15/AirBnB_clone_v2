cab8b5ad4b0f434b88fadc89e947b3c163d8d000
Israel <busygrr@gmail.com>
2024-08-22 21:29:39 -0700

    Task 0 Done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,16 +8,20 @@ from flask import Flask
 # Initialize the Flask application
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """Handles requests to the root URL and returns a welcome message."""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Handles requests to /hbnb URL and returns 'HBNB'."""
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    # Start the Flask application, making it accessible on all IP addresses at port 5000
+    # Start the Flask application, making it accessible on all IP addresses 
+    # at port 5000
     app.run(host='0.0.0.0', port=5000)
