c1139abeb64f600313a2de3b0765e407a9b89427
Israel Atuahene Addo (izy) <96962629+WMOriginalz15@users.noreply.github.com>
2024-08-23 03:08:57 +0000

    Task 0 Completed

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Flask application initialization script
+"""
+
+from flask import Flask
+
+# Instantiate the Flask application
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def homepage():
+    """Handles the root URL and returns a greeting."""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    # Run the application on all available IP addresses
+    app.run(host='0.0.0.0', port=5000)
