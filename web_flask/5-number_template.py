059eeeef00dd10a082e0de58f759c342dbe8faaa
Israel <busygrr@gmail.com>
2024-08-23 20:09:50 -0700

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,24 +5,28 @@ Flask web application module.
 
 from flask import Flask, render_template
 
-# Initialize the Flask application
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def welcome():
     """Respond with 'Hello HBNB!' at the root URL."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def show_hbnb():
     """Respond with 'HBNB'."""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     """Display 'C' followed by user-provided text, with underscores replaced by spaces."""
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -31,15 +35,18 @@ def display_python_text(text='is cool'):
     formatted_text = text.replace('_', ' ')
     return f'Python {formatted_text}'
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """Confirm and display that the provided value is a number."""
     return f'{n} is a number'
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_with_template(n):
     """Render an HTML page showing the provided number if it's an integer."""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     # Start the Flask application, listening on all IP addresses and port 5000
